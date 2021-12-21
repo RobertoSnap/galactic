69d3fe5d48bcd421d4afd40d705b87a3d2cc1a54
Robertosnap <robertosnap@pm.me>
2021-12-21 10:32:04 +0100

    Added last tilemap boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 use movement::MovementPlugin;
 use spawner::SpawnerPlugin;
+
 mod constants;
 mod movement;
 mod spawner;
@@ -31,12 +33,52 @@ fn main() {
         .run();
 }
 
-pub fn setup(mut commands: Commands) {
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // set window
     // let window = windows.get_primary_mut().unwrap();
     // window.set_position(IVec2::new(100, 100));
 
     // add resources
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.insert_resource(PlayerState { spaceship: None })
+    commands.insert_resource(PlayerState { spaceship: None });
+
+    // Map
+    let texture_handle = asset_server.load("Space/space_bg_3.png");
+    let map_entity = commands.spawn().id();
+    let mut map = Map::new(0u16, map_entity);
+    let layer_settings = LayerSettings::new(
+        MapSize(10, 10),
+        ChunkSize(64, 64),
+        TileSize(16.0, 16.0),
+        TextureSize(96.0, 16.0),
+    );
+    let center = layer_settings.get_pixel_center();
+    let (mut layer_builder, layer_entity) =
+        LayerBuilder::<TileBundle>::new(&mut commands, layer_settings, 0u16, 0u16);
+    // map.add_layer(&mut commands, 0u16, layer_entity);
+    // layer_builder.for_each_tiles_mut(|tile_entity, tile_data| {
+    //     // True here refers to tile visibility.
+    //     *tile_data = Some(TileBundle::default());
+    //     // Tile entity might not exist at this point so you'll need to create it.
+    //     if tile_entity.is_none() {
+    //         *tile_entity = Some(commands.spawn().id());
+    //     }
+    //     commands
+    //         .entity(tile_entity.unwrap())
+    //         .insert(LastUpdate::default());
+    // });
+
+    // map_query.build_layer(&mut commands, layer_builder, texture_handle);
+
+    // // Spawn Map
+    // // Required in order to use map_query to retrieve layers/tiles.
+    // commands
+    //     .entity(map_entity)
+    //     .insert(map)
+    //     .insert(Transform::from_xyz(-center.x, -center.y, 0.0))
+    //     .insert(GlobalTransform::default());
+}
+
+pub fn map(mut commands: Commands) {
+    todo!()
 }
